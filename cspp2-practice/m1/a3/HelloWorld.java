5de3956e50f1f67530e6a756f64dbac8dbdc7c47
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-27 21:34:24 +0530

    m1-a3

---
+++ cspp2-practice/m1/a3/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+  *First class in Java written.
+  *Author: Madhulika [20186015]
+  *Date: 27/08/2018
+*/
+
+final class HelloWorld {
+
+  /** Private constructor.
+  */
+  private HelloWorld() {
+    //abstraction
+  }
+
+  /**
+  First method in Java.
+  @param args of type String[]
+  */
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, World");
+  }
+}
