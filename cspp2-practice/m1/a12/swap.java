dac22ecb0c28b6e579620d29aa5dc85e45eb22e7
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-27 21:56:21 +0530

    m1-a12

--- cspp2-practice/m1/a12/swap.java
+++ cspp2-practice/m1/a12/swap.java
@@ -1,7 +1,23 @@
-
+import java.util.Scanner;
 
 public class swap {
 	public static void main(String[] args) {
+		int num1, num2, temp;
+
+		Scanner n1 = new Scanner(System.in);
+		System.out.println("Enter your first number");
+		num1 = n1.nextInt();
+
+		Scanner n2 = new Scanner(System.in);
+		System.out.println("Enter your second number");
+		num2 = n2.nextInt();
+
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+
+		System.out.println("The swapped numbers are " + num1 + ", " + num2);
+
 
 		
 	}
