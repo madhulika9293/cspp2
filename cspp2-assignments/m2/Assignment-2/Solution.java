c5f3560de5f66b0f4c6b6db8fa8b876a6bf9ccde
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-28 20:28:51 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -0,0 +1,62 @@
+/**
+  * Date: 28-08-2018.
+  * Name: Madhulika
+  * Roots to the quadratic equation
+*/
+
+import java.util.Scanner;
+
+/**
+  * The only class.
+*/
+
+public final class Solution {
+  /**
+    * default constructor.
+  */
+
+  private Solution() {
+    //unused
+  }
+  /**
+   * { calculates roots for quadratic equation }
+   *
+   * @param      x     { parameter_description }
+   * @param      y     { parameter_description }
+   * @param      z     { parameter_description }
+   */
+
+  public static void rootsOfQuadraticEquation
+    (int x, int y, int z) {
+
+      double delta = Math.pow(y, 2) - 4 * x * y;
+
+      if (delta < 0) {
+        System.out.println("Roots don't exist");
+      } else {
+        System.out.println((-y + delta)/(2*x));
+        System.out.println((-y - delta)/(2*x));
+      }
+
+  }
+  /*
+  Do not modify this main function.
+  */
+
+  /**
+   * { main function }
+   *
+   * @param      args  The arguments
+   */
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = scan.nextInt();
+    rootsOfQuadraticEquation(a, b, c);
+  }
+  /*
+  Need to write the rootsOfQuadraticEquation function and print the output.
+  */
+}
