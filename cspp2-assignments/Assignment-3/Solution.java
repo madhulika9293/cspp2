532bb0572b1d9c691d923809268ddb099b447d42
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-28 12:29:34 +0530

    4/4 Testcases Passed

--- cspp2-assignments/Assignment-3/Solution.java
+++ cspp2-assignments/Assignment-3/Solution.java
@@ -6,19 +6,45 @@
 
 import java.util.Scanner;
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int base = scan.nextInt();
-		int height = scan.nextInt();
-		areaOfTriangle(base, height);
-	}
+/**
+  * The only class.
+*/
 
-	/*
-	Need to fill the areaOfTriangle function and print the output
-	of fahrenheit.
-	*/
+public final class Solution {
+  /**
+    * default constructor.
+  */
+
+  private Solution() {
+    //unused
+  }
+
+  /**
+   * { function for area of a triangle }.
+   *
+   * @param      base    The base
+   * @param      height  The height
+   *
+   * @return     { area, area of the given triangle }
+   */
+
+  public static double areaOfTriangle(final double base, final double height) {
+    double area = 0.5 * base * height;
+    return area;
+  }
+
+  /**
+    Do not modify this main function.
+    * @param args is for command line arguments.
+  */
+
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int base = scan.nextInt();
+    int height = scan.nextInt();
+    areaOfTriangle(base, height);
+    System.out.println(areaOfTriangle(base, height));
+  }
+
 }
+
