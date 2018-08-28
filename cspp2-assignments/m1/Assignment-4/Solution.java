ff176fe85e45c7a48334adf2d5921d8da7241937
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-28 19:10:10 +0530

    M1-A4

---
+++ cspp2-assignments/m1/Assignment-4/Solution.java
@@ -0,0 +1,57 @@
+/**
+  * Date: 27-08-2018.
+  * Name: Madhulika
+  * Convert Fahrenheit temperature to Celsius
+*/
+
+import java.util.Scanner;
+
+/**
+  * The only class.
+*/
+
+public final class Solution {
+  /**
+    * default constructor.
+  */
+
+  private Solution() {
+    //unused
+  }
+
+  /*
+  Do not modify this main function.
+  */
+  /**
+   * Calculates the temperature from degrees to farenheit.
+   *
+   * @param      temp  Temperature
+   *
+   * @return     The temperature from farenheit to degrees.
+   */
+
+  public static void calculateFromDegreesToFarenheit(double temp) {
+    final double subNum = 32;
+    final double divNum = 1.8;
+
+    double cel;
+    cel = (temp - subNum) / divNum;
+    System.out.println(cel);
+  }
+
+  /**
+   * { main function }.
+   *
+   * @param      args  The arguments
+   */
+  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    double degreesCelsius = scan.nextDouble();
+    calculateFromDegreesToFarenheit(degreesCelsius);
+  }
+  /*
+  Need to fill the calculateFromDegreesToFarenheit function and print the output
+  of fahrenheit.
+  */
+}
