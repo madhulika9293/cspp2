5b069582d657af654a8e4db19681b903e6ff5135
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-28 18:40:13 +0530

    m2-a11

---
+++ cspp2-practice/m2/a11/factorial.java
@@ -0,0 +1,48 @@
+/**
+  * Date: 28-08-2018
+  * Name: Madhulika
+  * To compute the factoria of a given number
+ */
+
+import java.util.Scanner;
+
+/**
+  * The only class.
+*/
+
+public final class factorial {
+  /**
+    * default constructor.
+  */
+
+  private factorial() {
+    //unused
+  }
+
+  public static int fact(int x) {
+    if (x == 0 || x == 1) {
+      return 1;
+    } else {
+      return x * fact(x - 1);
+    }
+  }
+
+  /**
+   * {Main Function}.
+   *
+   * @param      args  The arguments
+   */
+
+  public static void main(final String[] args) {
+
+    System.out.println("Enter the number ");
+
+    Scanner scan = new Scanner(System.in);
+
+    int num = scan.nextInt();
+
+    System.out.println("The factorial of the given number is: " + fact(num));
+
+  }
+
+}
