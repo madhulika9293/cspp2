72d0594f226d5eea1d11161c5c49b968d51bee81
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-28 18:07:17 +0530

    m2-a10

---
+++ cspp2-practice/m2/a10/BiggerEqualSmaller.java
@@ -0,0 +1,32 @@
+/**
+  * Date: 28-08-2018
+  * Name: Madhulika
+  * Program to compare two numbers
+ */
+
+import java.util.Scanner;
+
+/**
+  * The only class.
+*/
+
+public final class BiggerEqualSmaller {
+  /**
+    * default constructor.
+  */
+
+  private BiggerEqualSmaller() {
+    //unused
+  }
+
+  /**
+   * {Main Function}.
+   *
+   * @param      args  The arguments
+   */
+
+  public static void main(final String[] args) {
+    
+  }
+
+}
