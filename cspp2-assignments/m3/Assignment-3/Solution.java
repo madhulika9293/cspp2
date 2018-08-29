0089a2d2863bb1ad3b2777372371f09f20015522
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-29 14:07:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -32,28 +32,28 @@ public final class Solution {
    * @return     GCD, int
    */
 
-  public static int gcd(int x, int y) {
-    int gcd_out = Math.min(x, y);
-    for (int i = gcd_out; i >= 1; i--) {
-      if (x%i == 0 && y%i == 0) {
+  public static int gcd(final int x, final int y) {
+    int gcdOut = Math.min(x, y);
+    for (int i = gcdOut; i >= 1; i--) {
+      if (x % i == 0 && y % i == 0) {
         return i;
       }
     }
     return 1;
   }
-  
+
   /**
    * Main function.
    *
    * @param      args  The arguments
    */
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
 
-    Scanner s=new Scanner(System.in);      
+    Scanner s = new Scanner(System.in); 
     int n1 = s.nextInt();
     int n2 = s.nextInt();
-    int gcd = gcd(n1,n2);
+    int gcd = gcd(n1, n2);
     System.out.println(gcd);
     }
   /*
