86fefefd0337a0b1821d79e40d7b818d81d9ad96
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-29 14:08:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -50,7 +50,7 @@ public final class Solution {
 
   public static void main(final String[] args) {
 
-    Scanner s = new Scanner(System.in); 
+    Scanner s = new Scanner(System.in);
     int n1 = s.nextInt();
     int n2 = s.nextInt();
     int gcd = gcd(n1, n2);
@@ -60,3 +60,4 @@ public final class Solution {
     Need to write the gcd function and print the output.
   */
 }
+
