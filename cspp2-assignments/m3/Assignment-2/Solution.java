0eb74f0536b775bbc1ef473992e23bede9bd0887
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-29 12:26:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -0,0 +1,41 @@
+/**
+  * Date: 29-08-2018.
+  * Name: Madhulika
+  * To print the number of 7's from 1 to n
+*/
+
+import java.util.Scanner;
+  /*
+  Do not modify this main function.
+  */
+
+  /**
+   * { main class }.
+   */
+public final class Solution {
+  /* Fill the main function to print the number of 7's between 1 to n*/
+  
+  /**
+  * { main function }.
+  *
+  * @param      args  The arguments
+  */
+
+  public static void main(final String[] args) {
+
+    Scanner s = new Scanner(System.in);      
+    int n = s.nextInt();
+    int count = 0;
+
+    for (int i = 1;i <= n ; i++) {
+      String num = String.valueOf(i);
+      char[] num_arr = num.toCharArray();
+      for (char c: num_arr) {
+        if (c == '7') {
+          count = count + 1;
+        }
+      }
+    }
+    System.out.println(count);
+  }
+}
