6d44fc8dd3977a2ce4ce2fac6e7849cf8e71fbbc
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-30 17:50:10 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -4,72 +4,72 @@
   * To find the largest number in a given array
 */
 
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Class for solution.
  */
 
-public class Solution {
+public final class Solution {
 
-	/**
-	* Constructs the object.
-	*/
+  /**
+  * Constructs the object.
+  */
 
-	private Solution() {
-		//unused
-	}
+  private Solution() {
+    //unused
+  }
 
-	/* Fill the main function to print
-	 * resultant of addition of matrices*/
+  /* Fill the main function to print
+   * resultant of addition of matrices*/
 
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
+  /**
+   * main function.
+   *
+   * @param      args  The arguments
+   */
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(final String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int n1 = sc.nextInt();
-		int n2 = sc.nextInt();
+    int n1 = sc.nextInt();
+    int n2 = sc.nextInt();
 
-		int[][] mat1 = new int[n1][n2];
+    int[][] mat1 = new int[n1][n2];
 
-		for (int i = 0; i < n1; i++) {
-			for (int j = 0; j < n2; j++) {
-				mat1[i][j] = sc.nextInt();
-			}
-		}
+    for (int i = 0; i < n1; i++) {
+      for (int j = 0; j < n2; j++) {
+        mat1[i][j] = sc.nextInt();
+      }
+    }
 
-		int n3 = sc.nextInt();
-		int n4 = sc.nextInt();
+    int n3 = sc.nextInt();
+    int n4 = sc.nextInt();
 
-		int[][] mat2 = new int[n3][n4];
+    int[][] mat2 = new int[n3][n4];
 
-		for (int i = 0; i < n3; i++) {
-			for (int j = 0; j < n4; j++) {
-				mat2[i][j] = sc.nextInt();
-			}
-		}
-		if (n1 == n3 && n2 == n4) {
-		int[][] matAdd = new int[n1][n2];
+    for (int i = 0; i < n3; i++) {
+      for (int j = 0; j < n4; j++) {
+        mat2[i][j] = sc.nextInt();
+      }
+    }
+    if (n1 == n3 && n2 == n4) {
+    int[][] matAdd = new int[n1][n2];
 
-		for (int i = 0; i < n1; i++) {
-			String output = "";
-			for (int j = 0; j < n2; j++) {
-				matAdd[i][j] = mat1[i][j] + mat2[i][j];
-				output += matAdd[i][j];
-				output += " ";
-			}
-			System.out.println(output.substring(0,output.length()-1));
-		}			
-		} else {
-			System.out.println("not possible");
-		}
-	}
+    for (int i = 0; i < n1; i++) {
+      String o = "";
+      for (int j = 0; j < n2; j++) {
+        matAdd[i][j] = mat1[i][j] + mat2[i][j];
+        o += matAdd[i][j];
+        o += " ";
+      }
+      System.out.println(o.substring(0, o.length() - 1));
+    }
+    } else {
+      System.out.println("not possible");
+    }
+  }
 }
 
 
