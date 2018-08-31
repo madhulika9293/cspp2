91f16f1843d62f9f4f0f47e6c8810fa861ddb989
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-31 10:26:18 +0530

    m4-a12

---
+++ cspp2-practice/m4/a12/Concatinate.java
@@ -0,0 +1,37 @@
+/**
+  * Date: 31-08-2018.
+  * Name: Madhulika
+  * To concatinate 2 strings and print out the output
+*/
+
+import java.util.Scanner;
+
+/**
+ * The only class.
+ */
+
+public final class Concatinate {
+  /**
+   * input array.
+   */
+
+  /**
+   * Constructs the object.
+   */
+
+  private Concatinate() {
+    //unused
+    }
+
+  /**
+   * main function.
+   *
+   * @param      args  The arguments
+   */
+
+  public static void main(final String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+  }
+}
