36472e63180d14d958f94b68daf2515d7bd64526
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-01 09:54:24 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -37,7 +37,7 @@ public final class Solution {
    */
   static void oddComposites(final int n) {
     // write your code here
-    for (int i = 3; i <= n; i = i + 2) {
+    for (int i = 0; i <= n; i = i + 2) {
       if (!isPrime(i)) {
         System.out.println(i);
       }
