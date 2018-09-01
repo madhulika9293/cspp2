4ef1c0360977bc24f73662476a1bf32a572379ac
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-01 09:49:36 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -6,27 +6,50 @@ import java.util.Scanner;
  * @author : Madhulika, 20186015
  */
 public final class Solution {
-    /**
-    * Empty constructor.
-    */
-    private Solution() {
-        //not used
+  /**
+  * Empty constructor.
+  */
+  private Solution() {
+    //not used
+  }
+
+  /**
+   * Determines if prime.
+   *
+   * @param      num   The number
+   *
+   * @return     True if prime, False otherwise.
+   */
+  
+  public static boolean isPrime(final int num) {
+    for (int i = 2; i < num; i++) {
+      if (num%i == 0) {
+        return false;
+      }
     }
-    /**
-     * Prints the odd composite numbers between 2 and n.
-     *
-     * @param      n     n value
-     */
-    static void oddComposites(final int n) {
-  // write your code here
+    return true;
+  }
+
+  /**
+   * Prints the odd composite numbers between 2 and n.
+   *
+   * @param      n     n value
+   */
+  static void oddComposites(final int n) {
+    // write your code here
+    for (int i = 3; i <= n; i = i + 2) {
+      if (isPrime(i) == false) {
+        System.out.println(i);
+      }
     }
-    /**
-    * main method as driver program.
-    * @param args is the parameter for this method
-    */
-    public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        oddComposites(n);
-    }
+  }
+  /**
+  * main method as driver program.
+  * @param args is the parameter for this method
+  */
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    oddComposites(n);
+  }
 }
