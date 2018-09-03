9926b0bf023ed18960f9aa0113b1da1ef4f43259
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-03 19:07:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -0,0 +1,42 @@
+/**
+  * Date: 03-09-2018.
+  * Name: Madhulika
+  * To validate the given input
+*/
+
+import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
+class InputValidator {
+  /*Write the atrributes and methods for InputValidator*/
+  /**
+   * input string.
+   */
+  private String inpData;
+
+  /**
+   * Constructs the object.
+   *
+   * @param      iD    input string
+   */
+
+  public InputValidator(final String iD) {
+    inpData = iD;
+  }
+
+  public boolean validateData() {
+    return inpData.length() >= 6;
+  }
+
+}
+public class Solution {
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    String input = s.next();
+    InputValidator i = new InputValidator(input);
+    System.out.println(i.validateData());
+
+  }
+
+}
