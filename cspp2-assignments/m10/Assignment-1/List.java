9d245ce7319315abf085449079eb1148735e044f
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-06 13:57:11 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -323,8 +323,9 @@ public class List {
           if (t.length == 1) {
             l.add(Integer.parseInt(tokens[1]));
           } else {
-            if (t.length > 1)
+            if (t.length > 1){
               l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
+            }
           }
         }
         break;
@@ -334,7 +335,7 @@ public class List {
       case "addAll":
         if (tokens.length == 2) {
           String[] t1 = tokens[1].split(",");
-          int temp[] = new int[t1.length];
+          int[] temp = new int[t1.length];
           for (int i = 0; i < temp.length; i++) {
             temp[i] = Integer.parseInt(t1[i]);
           }
@@ -365,6 +366,8 @@ public class List {
       case "contains":
         System.out.println(l.contains(Integer.parseInt(tokens[1])));
         break;
+      default:
+        //unused
       }
     }
   }
