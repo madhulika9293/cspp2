7b8c7441e224368f8415bb98067901d70e70ccbf
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-07 12:09:07 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -2,6 +2,9 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
 
+/**
+ * List ADT.
+ */
 public class List {
   //Implement all the methods mentioned to build a ListADT
 
