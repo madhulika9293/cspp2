cceb91a935a091e6591aab30b1b5d3ad39c55d84
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-07 14:39:18 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-2/Solution.java
+++ cspp2-assignments/m11/Assignment-2/Solution.java
@@ -62,7 +62,7 @@ class Student {
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 
   /**
    * Constructs the object.
@@ -429,15 +429,17 @@ public class Solution {
           break;
           }
         String[] arrstring3 = tokens[1].split(",");
-        List object = l.subList(Integer.parseInt(arrstring3[0]), Integer.parseInt(arrstring3[1]));
-        if (object != null)
+        List object = l.subList(Integer.parseInt(arrstring3[0]),
+          Integer.parseInt(arrstring3[1]));
+        if (object != null) {
           System.out.println(object);
+        }
         break;
       case "equals":
         if (tokens.length == 2) {
           String[] lt = tokens[1].split(",");
           List l2 = new List();
-          for (int k = 0; k < lt.length; k++ ) {
+          for (int k = 0; k < lt.length; k++) {
             l2.add((lt[k]));
           }
           System.out.println(l.equals(l2));
@@ -451,8 +453,12 @@ public class Solution {
       }
     }
   }
-
-  public static void executeListStudent(Scanner stdin) {
+  /**
+   * Student class callable.
+   *
+   * @param      stdin  The stdin
+   */
+  public static void executeListStudent(final Scanner stdin) {
     List<Student> l = new List();
     while (stdin.hasNext()) {
       // read the line
@@ -499,7 +505,7 @@ public class Solution {
         if (tokens.length == 2) {
           String[] t1 = tokens[1].split(",");
           Student[] students = new Student[t1.length];
-          for (int i = 0; i < t1.length ; i++) {
+          for (int i = 0; i < t1.length; i++) {
             students[i] = new Student(t1[i]);
           }
           l.addAll(students);
@@ -509,25 +515,26 @@ public class Solution {
         if (tokens.length == 2) {
           String[] t1 = tokens[1].split(",");
           Student[] students = new Student[t1.length];
-          for (int i = 0; i < t1.length ; i++) {
+          for (int i = 0; i < t1.length; i++) {
             students[i] = new Student(t1[i]);
           }
           l.removeAll(students);
         }
         break;
-      case "subList": {
-        if (tokens.length != 2) break;
+      case "subList":
+        if (tokens.length != 2) {
+          break;
+        }
         String[] arrstring3 = tokens[1].split(",");
         List object = l.subList(Integer.parseInt(arrstring3[0]), Integer.parseInt(arrstring3[1]));
         if (object != null)
           System.out.println(object);
         break;
-      }
       case "equals":
         if (tokens.length == 2) {
           String[] lt = tokens[1].split(",");
           List l2 = new List();
-          for (int k = 0; k < lt.length; k++ ) {
+          for (int k = 0; k < lt.length; k++) {
             l2.add((lt[k]));
           }
           System.out.println(l.equals(l2));
@@ -546,7 +553,7 @@ public class Solution {
    *
    * @param      args  The arguments
    */
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     // create an object of the list to invoke methods on it
     Scanner stdin = new Scanner(new BufferedInputStream(System.in));
     String objectType = stdin.nextLine();
@@ -572,7 +579,9 @@ public class Solution {
       executeListDouble(stdin);
       break;
 
-    case "O"://This case will be executed for Student type list i.e to store List of Student Objects
+    case "O":
+    //This case will be executed for Student type list
+    // i.e to store List of Student Objects
       executeListStudent(stdin);
       break;
     default:
