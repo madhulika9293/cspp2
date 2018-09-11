7d565d7ab9bba922fadd895a733c5a7e5f86e75e
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-11 19:08:06 +0530

    "check_style_score -> 0 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m15/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Solution.java
@@ -76,7 +76,6 @@ class SortedSet extends Set {
   public void last() {
     if (size == 0) {
       System.out.println("Set Empty Exception");
-      System.out.println("-1");
     } else {
       System.out.println(setArr[size - 1]);
     }
