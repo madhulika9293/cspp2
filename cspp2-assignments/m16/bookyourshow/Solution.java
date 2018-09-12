4204c7649f25974ff69c2999b236cdbcf3a2daf6
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-12 15:51:26 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m16/bookyourshow/Solution.java
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -185,9 +185,8 @@ class BookYourShow {
     for (String seat : seats) {
       for (int i = 0; i < seats.length; i++) {
         if (seat.equals(sseats[i])) {
-          show.setSeats(i);
-          System.out.println(i);
-          System.out.println(sseats[i]);
+          int temp = i;
+          show.setSeats(temp);
         }
         flagCheck = true;
       }
