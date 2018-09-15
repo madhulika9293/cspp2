51523331b905569b00f579a38bf6e4b4409a1572
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-15 13:08:14 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -118,20 +118,21 @@ public final class Solution {
     // write your code here to display the quiz questions
     // read the user responses from the console
     // store the user respones in the quiz object
-    int i = 0;
-    for (question q : quiz.getQList()) {
+    // int i = 0;
+    // for (question q : quiz.getQList()) {
 
-        quiz.getQ(i).print();
-        String[] tempAns = s.nextLine().split(" ");
-        quiz.setA(i, tempAns[1]);
-      i += 1;
-    }
-    // for (int i = 0; i < answerCount; i++) {
-    //   quiz.getQ(i).print();
+    //   // quiz.getQ(i).print();
+    //   q.print();
     //   String[] tempAns = s.nextLine().split(" ");
     //   quiz.setA(i, tempAns[1]);
-    //   System.out.println(quiz.getA(i));
+    //   i += 1;
     // }
+    for (int i = 0; i < answerCount; i++) {
+      quiz.getQ(i).print();
+      String[] tempAns = s.nextLine().split(" ");
+      quiz.setA(i, tempAns[1]);
+      System.out.println(quiz.getA(i));
+    }
 
   }
 
