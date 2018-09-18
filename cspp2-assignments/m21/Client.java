ad5954935e83d33f5a70287f48aea4e58e80abb0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-18 19:08:14 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ cspp2-assignments/m21/Client.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		fitbyte person = new fitbyte();
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] inputs = line.split(" ");
+			switch (inputs[0]) {
+			case "Food":
+				person.addFood(new foodlog(inputs[1].split(",")));
+				break;
+			case "Water":
+				person.addWater(new waterlog(inputs[1].split(",")));
+				break;
+			case "PhysicalActivity":
+				person.addActivity(new activitylog(inputs[1].split(",")));
+				break;
+			case "Weight":
+				person.addWeight(new weightlog(inputs[1].split(",")));
+				break;
+			case "Sleep":
+				person.addSleep(new sleeplog(inputs[1].split(",")));
+				break;
+			case "Foodlog":
+				person.foodSumm();
+				System.out.println("\n");
+				break;
+			case "Waterlog":
+				person.waterSumm();
+				System.out.println("\n");
+				break;
+			case "PhysicalActivitylog":
+				person.activitySumm();
+				System.out.println("\n");
+				break;
+			case "Weightlog":
+				person.weightSumm();
+				System.out.println("\n");
+				break;
+			case "Sleeplog":
+				person.sleepSumm();
+				System.out.println("\n");
+				break;
+			case "Summary":
+				if (inputs.length == 1) {
+					person.summary();
+				} else {
+					person.summary(inputs[1]);
+				}
+				break;
+			default:
+				break;
+			}
+
+		}
+	}
+}
+
+
---
+++ cspp2-assignments/m21/fitbyte.java
@@ -0,0 +1,225 @@
+import java.util.ArrayList;
+
+class foodlog {
+
+	private String date, name, time, quantity;
+
+	foodlog(final String[] arg) {
+
+		name = arg[0];
+		quantity = arg[1];
+		date = arg[2];
+		time = arg[3];
+	}
+
+	public String getdate() {
+		return date;
+	}
+
+
+	public String toString() {
+		return "Food" + "\n" + "Date:" + date + "\n" + "Time:" + time + "\n"
+		       + "Name:" + name + "\n" + "Quantity:" + quantity;
+	}
+
+}
+
+class waterlog {
+
+	private String quantity, date, time;
+
+	waterlog(final String[] arg) {
+
+		date = arg[1];
+		time = arg[2];
+		quantity = arg[0];
+	}
+
+	public String getdate() {
+		return date;
+	}
+
+	public String toString() {
+		return "Water" + "\n" + "Date:" + date + "\n" + "Quantity:"
+		       + quantity;
+	}
+
+}
+
+class activitylog {
+
+	private String date, name, startTime, endTime, notes;
+
+	activitylog(final String[] arg) {
+
+		date = arg[2];
+		name = arg[0];
+		startTime = arg[3];
+		endTime = arg[4];
+		notes = arg[1];
+	}
+
+	public String getdate() {
+		return date;
+	}
+
+	public String toString() {
+		return "PhysicalActivity" + "\n" + "Name:" + name + "\n" + "Notes:" + notes + "\n"
+		       + "Date:" + date + "\n" + "Starttime:" + startTime + "\n"
+		       + "Endtime:" + endTime;
+	}
+
+}
+
+class sleeplog {
+	private String date, startTime, endTime;
+
+	sleeplog(final String[] arg) {
+		date = arg[0];
+		startTime = arg[1];
+		endTime = arg[2];
+	}
+
+	public String getdate() {
+		return date;
+	}
+
+	public String toString() {
+		return "Sleep" + "\n" + "Date:" + date + "\n"
+		+ "Starttime:" + startTime + "\n" + "Endtime:" + endTime;
+	}
+}
+
+class weightlog {
+	private String date, time, weight, fatPer;
+
+	weightlog(final String[] arg) {
+		date = arg[0];
+		time = arg[1];
+		weight = arg[2];
+		fatPer = arg[3];
+	}
+
+	public String getdate() {
+		return date;
+	}
+
+	public String toString() {
+		return "Weight" + "\n" + "Date:" + date + "\n" + "Time:" + time + "\n"
+		       + "Weight:" + weight + "\n" + "Fat:" + fatPer;
+	}
+}
+
+public class fitbyte {
+	private ArrayList<foodlog> food;
+	private ArrayList<waterlog> water;
+	private ArrayList<activitylog> activity;
+	private ArrayList<sleeplog> sleep;
+	private ArrayList<weightlog> weight;
+
+	fitbyte() {
+		food = new ArrayList<>();
+		water = new ArrayList<>();
+		activity = new ArrayList<>();
+		sleep = new ArrayList<>();
+		weight = new ArrayList<>();
+	}
+
+	public void addFood(final foodlog item) {
+		food.add(item);
+	}
+
+	public void addWater(final waterlog item) {
+		water.add(item);
+	}
+
+	public void addActivity(final activitylog item) {
+		activity.add(item);
+	}
+
+	public void addSleep(final sleeplog item) {
+		sleep.add(item);
+	}
+
+	public void addWeight(final weightlog item) {
+		weight.add(item);
+	}
+
+	public void foodSumm() {
+		for (foodlog entry : food) {
+			System.out.println(entry);
+		}
+	}
+
+	public void waterSumm() {
+		for (waterlog entry : water) {
+			System.out.println(entry);
+		}
+	}
+
+	public void activitySumm() {
+		for (activitylog entry : activity) {
+			System.out.println(entry);
+		}
+	}
+
+	public void sleepSumm() {
+		for (sleeplog entry : sleep) {
+			System.out.println(entry);
+		}
+	}
+
+	public void weightSumm() {
+		for (weightlog entry : weight) {
+			System.out.println(entry);
+		}
+	}
+
+	public void summary() {
+		for (foodlog entry : food) {
+			System.out.println(entry);
+		}
+		for (waterlog entry : water) {
+			System.out.println(entry);
+		}
+		for (sleeplog entry : sleep) {
+			System.out.println(entry);
+		}
+		for (activitylog entry : activity) {
+			System.out.println(entry);
+		}
+		for (weightlog entry : weight) {
+			System.out.println(entry);
+		}
+	}
+
+	public void summary(final String day) {
+		for (foodlog entry : food) {
+			if (entry.getdate().equals(day)) {
+				System.out.println(entry);
+			}
+		}
+		for (waterlog entry : water) {
+			if (entry.getdate().equals(day)) {
+				System.out.println(entry);
+			}
+		}
+		for (sleeplog entry : sleep) {
+			if (entry.getdate().equals(day)) {
+				System.out.println(entry);
+			}
+		}
+		for (activitylog entry : activity) {
+			if (entry.getdate().equals(day)) {
+				System.out.println(entry);
+			}
+		}
+		for (weightlog entry : weight) {
+			if (entry.getdate().equals(day)) {
+				System.out.println(entry);
+			}
+		}
+	}
+
+
+}
